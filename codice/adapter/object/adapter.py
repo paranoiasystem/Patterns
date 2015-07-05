93d947af050beaca23a4cc6ff1fea22a684c585e
ParanoiaSystem <m.ferraioli25@studenti.unisa.it>
2015-07-05 22:45:25 +0200

    object adapter

---
+++ codice/adapter/object/adapter.py
@@ -0,0 +1,49 @@
+class Impiegato(object):
+	
+	__cognome = None
+
+	def __init__(self):
+		super().__init__()
+	
+	def	setCognome(self, cognome):
+		self.__cognome = cognome
+
+	def getCognome(self):
+		return self.__cognome
+
+class Employer(object):
+
+	__lastName = None
+
+	def __init__(self):
+		super().__init__()
+
+	def setLastName(self, lastName):
+		self.__lastName = lastName
+
+	def getLastName(self):
+		return self.__lastName
+
+class AdattatoreEmployer(Employer):
+	employer = None
+	def __init__(self, employer):
+		super().__init__()
+		self.employer = employer
+
+	def getCognome(self):
+		return self.employer.getLastName()
+
+	def setCognome(self, cognome):
+		self.employer.setLastName(cognome)
+		
+def main():
+	impiegato = Impiegato()
+	impiegato.setCognome("Rossi")
+	print(impiegato.getCognome())
+
+	adattatoreemployer = AdattatoreEmployer(Employer())
+	adattatoreemployer.setCognome("Simpson")
+	print(adattatoreemployer.getCognome())
+
+if __name__ == '__main__':
+	main()
